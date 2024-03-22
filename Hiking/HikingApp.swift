bd1bec94d26936c977677d950cb7101fd9c31cb4
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-03-22 19:22:12 +0300

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Hasan on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
